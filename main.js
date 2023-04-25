e1c79a1512844db0cb57f14c9fb18d905b65bede
mgballou <matthewbuiltthat@gmail.com>
2023-04-25 01:02:02 -0400

    Additional styling, home section, skills section

--- main.js
+++ main.js
@@ -2,16 +2,41 @@ console.log('JS linked')
 
 // cached DOM elements
 const navbarEl = document.querySelector('nav')
+const navbarMenuEl = document.getElementById('navbar-target')
 
 // event listeners
 
-navbarEl.addEventListener('click', scrollToElement)
+navbarMenuEl.addEventListener('click', scrollToElement)
 
+navbarEl.addEventListener('mouseover', displayNav)
+
+navbarEl.addEventListener('mouseout', resetOpacity)
 
 // functions
 
-function scrollToElement(evt){
-    if (evt.target.id !== ''){
+function scrollToElement(evt) {
+    if (evt.target.id !== '' && evt.target.id !== 'navbar-target') {
         document.getElementById(`${evt.target.id}-section`).scrollIntoView({ behavior: 'smooth', block: 'center' })
+    }
+}
+
+function displayNav(evt) {
+    if (window.scrollY > 35) {
+        navbarEl.style.opacity = "0.8"
+    }
+}
+
+function resetOpacity(evt){
+    if (window.scrollY > 35){
+        navbarEl.style.opacity = "0.0"
+    }
+}
+// window events
+
+window.onscroll = function (evt) {
+    if (window.scrollY <= 35) {
+        navbarEl.style.opacity = "1"
+    } else {
+        navbarEl.style.opacity = "0.0"
     }
 }
